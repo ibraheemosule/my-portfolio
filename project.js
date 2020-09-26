61b02687a7a2c9dd84b029494b2f277911cf9d1b
Ibrahim Sule <sulayibraheem@gmail.com>
2020-09-26 10:58:08 +0100

    adjusted the slider

--- project.js
+++ project.js
@@ -22,19 +22,22 @@ document.addEventListener('click', function (event) {
     content.classList.toggle('active');
 });
 
+
 let button = document.getElementById('menu');
+let button2 = document.getElementById('menu2');
             let li = document.querySelectorAll('li');
             let ul = document.getElementsByTagName('ul')[0];
             let nav = document.getElementsByTagName('nav')[0];
 
             const active = () => {
                 if(window.matchMedia("(max-width: 500px)").matches){
-                button.style.visibility = "hidden";
+                button.style.display = "none";
+                button2.style.display = "inline-block";
                 button.style.fontSize = "100%";
                 ul.style.paddingRight = "0px";
                 ul.style.paddingLeft = "0px";
                 ul.style.opacity="0";
-                ul.style.backgroundColor = "none";
+                ul.style.backgroundColor = "rgba(128, 128, 128, 0.693)";
                 for (let index = 0; index < 4; index++) {
                 li[index].style.display="block";
                 li[index].style.opacity="0";
@@ -51,27 +54,54 @@ let button = document.getElementById('menu');
                         li[2].style.opacity = index;
                         li[3].style.opacity = index;
 
+                        index += 0.2;
+                    }
+                }, 10);
+                };
+            }
+            button.addEventListener('click', active);
+
+            const inactive = () => {
+                if(window.matchMedia("(max-width: 500px)").matches){
+                    button2.style.display = "none";
+                button.style.display = "inline-block";
+                    button.style.fontSize = "250%";
+                ul.style.paddingRight = "2%";
+                ul.style.paddingLeft = "2%";
+                // for (let index = 0; index < 4; index++) {
+                // li[index].style.display="none";
+                // }
+
+                let index = 1;
+                setInterval(() => {
+                    if(index >= 0){
+                        ul.style.opacity="initial";
+                        li[0].style.opacity = index;
+                        li[1].style.opacity = index;
+                        li[2].style.opacity = index;
+                        li[3].style.opacity = index;
+
                         li[0].style.backgroundColor = "rgba(128, 128, 128, 0.693)";
                         li[1].style.backgroundColor = "rgba(128, 128, 128, 0.693)";
                         li[2].style.backgroundColor = "rgba(128, 128, 128, 0.693)";
                         li[3].style.backgroundColor = "rgba(128, 128, 128, 0.693)";
 
-                        index += 0.2;
-                    }
-                }, 5);
-                };
-            }
-            nav.addEventListener('mouseover', active);
+                        index -= 0.1;
 
-            const inactive = () => {
-                if(window.matchMedia("(max-width: 500px)").matches){
-                    button.style.visibility = "visible";
-                    button.style.fontSize = "250%";
-                ul.style.paddingRight = "2%";
-                ul.style.paddingLeft = "2%";
-                for (let index = 0; index < 4; index++) {
-                li[index].style.display="none";
-                }
+                        if (index <=0.9 ) {
+                            li[3].style.display="none";
+                        }
+                        if (index <=0.6 ) {
+                            li[2].style.display="none";
+                        }
+                        if (index <=0.3 ) {
+                            li[1].style.display="none";
+                        }
+                        if (index <=0 ) {
+                            li[0].style.display="none";
+                        }
+                    }
+                }, 10);
                 };
             }
-            nav.addEventListener('mouseout', inactive);
+            button2.addEventListener('click', inactive);
